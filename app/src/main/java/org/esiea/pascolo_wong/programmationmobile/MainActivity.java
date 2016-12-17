1697eda267784d20652c94dce785f8be7f22c3a9
Rémi <rempascolo@gmail.com>
2016-12-17 20:45:16 +0100

    Starting Activity Layout

--- app/src/main/java/org/esiea/pascolo_wong/programmationmobile/MainActivity.java
+++ app/src/main/java/org/esiea/pascolo_wong/programmationmobile/MainActivity.java
@@ -2,6 +2,10 @@ package org.esiea.pascolo_wong.programmationmobile;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView tv_helloworld = (TextView)findViewById(R.id.tv_welcome);
+        /*RecyclerView rv = (RecyclerView) findViewById(R.id.rv_list);
+        rv.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false));*/
+    }
+
+    /*public void showToast(View v)
+    {
+        Toast.makeText(getApplicationContext(),getString(R.string.msg), Toast.LENGTH_SHORT).show();
+    }*/
+
+    public void onDownload (View v)
+    {
+        Toast.makeText(getApplicationContext(),getString(R.string.downloadToast), Toast.LENGTH_SHORT).show();
     }
 }
