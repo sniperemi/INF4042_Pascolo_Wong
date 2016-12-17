5b39c812be431b2860ca9f6af2f3a86918a39c63
Rémi <rempascolo@gmail.com>
2016-12-17 21:58:04 +0100

    Download Notification and Application Icon

--- app/src/main/java/org/esiea/pascolo_wong/programmationmobile/MainActivity.java
+++ app/src/main/java/org/esiea/pascolo_wong/programmationmobile/MainActivity.java
@@ -1,7 +1,10 @@
 package org.esiea.pascolo_wong.programmationmobile;
 
+import android.app.NotificationManager;
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.NotificationCompat;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -13,8 +16,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        TextView tv_helloworld = (TextView)findViewById(R.id.tv_welcome);
-        /*RecyclerView rv = (RecyclerView) findViewById(R.id.rv_list);
+        /*TextView tv_helloworld = (TextView)findViewById(R.id.tv_welcome);
+        RecyclerView rv = (RecyclerView) findViewById(R.id.rv_list);
         rv.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false));*/
     }
 
@@ -26,5 +29,16 @@ public class MainActivity extends AppCompatActivity {
     public void onDownload (View v)
     {
         Toast.makeText(getApplicationContext(),getString(R.string.downloadToast), Toast.LENGTH_SHORT).show();
+        downloadNotification();
+    }
+
+    private void downloadNotification ()
+    {
+        NotificationCompat.Builder notifBuilder = (NotificationCompat.Builder) new NotificationCompat.Builder(this)
+                .setSmallIcon(R.drawable.download)
+                .setContentTitle(getString(R.string.colorPack))
+                .setContentText(getString(R.string.downloading));
+        NotificationManager notifMnger = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        notifMnger.notify(0, notifBuilder.build());
     }
 }
