e9320cdb238465e9c623a75a107ae91438fc3048
Rémi <rempascolo@gmail.com>
2016-12-19 02:57:36 +0100

    Color from hexa, RGB buttons deleted

--- app/src/main/java/org/esiea/pascolo_wong/programmationmobile/activity_hexa_from_color_db.java
+++ app/src/main/java/org/esiea/pascolo_wong/programmationmobile/activity_hexa_from_color_db.java
@@ -13,17 +13,12 @@ import android.os.Bundle;
 import android.support.v7.app.NotificationCompat;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.util.AttributeSet;
-import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
 import android.widget.Toast;
 
 import org.json.JSONArray;
-import org.json.JSONObject;
 import org.json.JSONException;
 
 import java.io.FileInputStream;
